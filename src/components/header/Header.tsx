c7ee0991387c74347d450efe31a0d80c5188e736
bobby <rlatnguscjsw@gmail.com>
2024-05-21 01:22:26 +0900

    Feat : Header Components
    - Header Components 생성
    - DesktopHeader Component
    - MobileHeader Component
    
    Feat     : 새로운 기능 추가
    Refactor : 코드 리펙토링
    Fix      : 버그 수정
    Style    : CSS 수정
    Docs     : 문서 추가 및 수정
    Rename   : 폴더 이름 수정 및 위치 수정
    Remove   : 폴더 삭제

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,20 @@
 "use client";
+import React from "react";
 import Link from "next/link";
-import React, { useState } from "react";
+
+import MobileHeader from "@/components/header/MobileHeader";
+import DesktopHeader from "@/components/header/DesktopHeader";
 
 const Header: React.FC = () => {
-  const [btn, setBtn] = useState(false);
   return (
-    <header className="sticky top-0 right-0 left-0 bg-white p-5 z-50">
-      <button className="border-black" onClick={() => setBtn(true)}>
-        리스트 버튼
-      </button>
+    <header className="text-lg font-extrabold header sticky top-0 px-8 md:px-16 bg-white shadow-md flex items-center justify-between z-50">
+      <h1 className="w-3/12">
+        <Link href="/">Mento</Link>
+      </h1>
 
-      <div className={`relative flex `}>
-        <button className={`fixed top-0 left-0 right-0 bottom-0 transition-all duration-300 ${btn ? "bg-[#80808060] z-10 cursor-pointer" : "-z-10 cursor-default"}`} onClick={() => setBtn(false)} />
+      <DesktopHeader />
 
-        <div className={`fixed top-0 left-0 z-20 w-64 h-full transition-all duration-300 transform -translate-x-full bg-white shadow-lg ${btn && "translate-x-0"}`}>
-          <button onClick={() => setBtn(false)}>닫기</button>
-          <div className="px-6 py-4">
-            <h2 className="text-lg font-semibold">Drawer</h2>
-            <p className="text-gray-500">This is a drawer.</p>
-          </div>
-
-          <Link href={"/home"}>1</Link>
-        </div>
-      </div>
+      <MobileHeader />
     </header>
   );
 };
